e1a5c4649ff2f74ca39f3b398207448078269d80
strange-tiger <jinolee9525@gmail.com>
2026-09-06 15:14:45 +0900

    Feat: #3.1 HTML in Javascript
    어떻게 Javascript에서 HTML을 변경하는가? #1

--- momentum/app.js
+++ momentum/app.js
@@ -1,24 +1,5 @@
-const calculator =
-{
-    add: function (a, b)
-    {
-        console.log(a + b);
-    },
-    minus: function (a, b)
-    {
-        console.log(a - b);
-    },
-    divide: function (a, b)
-    {
-        console.log(a / b);
-    },
-    powerof: function (a, b)
-    {
-        console.log(a ** b);
-    }
-};
+const title = document.getElementById("title");
 
-calculator.add(5, 3);
-calculator.minus(5, 3);
-calculator.divide(5, 3);
-calculator.powerof(5, 3);
+title.innerText = "Got you!";
+console.log(title.id);
+console.log(title.className);
