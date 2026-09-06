8853c3b8ebaabfd16714a57e4688d96322aee4f1
strange-tiger <jinolee9525@gmail.com>
2026-09-06 15:29:33 +0900

    Feat: #3.2 Searching For Elements
    querySelector는 element를 CSS처럼 검색할 수 있다.
    querySelector는 CSS Selector처럼 사용
    querySelector는 맨처음 요소만 받음 - 전부 필요하면 querySelectorAll로 array 받아서 사용

--- momentum/app.js
+++ momentum/app.js
@@ -1,5 +1,6 @@
-const title = document.getElementById("title");
+let title = document.querySelector("#hello"); // ID로 찾기
+title = document.querySelector(".hello h1"); // class로 찾기
+// querySelector는 CSS Selector처럼 사용
+// querySelector는 맨처음 요소만 받음 - 전부 필요하면 querySelectorAll로 array 받아서 사용
 
-title.innerText = "Got you!";
-console.log(title.id);
-console.log(title.className);
+title.innerText = "Hello";
