11754b899d9aea4c0739ad959ab7ab0c345fded3
strange-tiger <jinolee9525@gmail.com>
2026-09-06 21:37:25 +0900

    Feat: #3.6~#3.8 javascript와 css 연결
    JS와 CSS를 연결 짓는다.
    스타일은 CSS에 적는 것이 좋다.
    JS에서 HTML의 클래스를 바꾸면 이전 클래스 명이 지워진다.
    -> classList로 해결 가능
    toggle 함수로 클래스명 지웠다 넣었다 가능 - classList에 클래스명이 있는지 확인 후 있으면 지우고 없으면 추가함

--- momentum/app.js
+++ momentum/app.js
@@ -2,39 +2,8 @@ let h1 = document.querySelector(".hello h1"); // class로 찾기
 
 function handleh1Click()
 {
-    h1.style.color = "blue";
-}
-function handleMouseEnter()
-{
-    h1.innerText = "Mouse is here!";
-}
-function handleMouseLeave()
-{
-    h1.innerText = "Mouse is gone!"
-}
-function handleWindowResize()
-{
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy()
-{
-    alert("copier!");
-}
-function handleWindowOffline()
-{
-    alert("SOS no WIFI!");
-}
-function handleWindowOnline()
-{
-    alert("All Goood");
+    const clickedClass = "clicked";
+    h1.classList.toggle(clickedClass);
 }
 
-h1.onclick = handleh1Click;
-h1.onmouseenter= handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-h1.removeEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleh1Click);
