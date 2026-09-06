bbb7cd8e03ba7134da70d04191408744de7bb9f2
strange-tiger <jinolee9525@gmail.com>
2026-09-06 12:50:08 +0900

    Feat: recap - 2.1~2.10 요약 과제
    변수 타입과 배열, object, 함수
    계산기 만들기 과제

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,24 @@
-alert("hi");
+const calculator =
+{
+    add: function (a, b)
+    {
+        console.log(a + b);
+    },
+    minus: function (a, b)
+    {
+        console.log(a - b);
+    },
+    divide: function (a, b)
+    {
+        console.log(a / b);
+    },
+    powerof: function (a, b)
+    {
+        console.log(a ** b);
+    }
+};
+
+calculator.add(5, 3);
+calculator.minus(5, 3);
+calculator.divide(5, 3);
+calculator.powerof(5, 3);
