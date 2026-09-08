dd35a1fb3eaf7ec7c1d8d502cf334a8882271a31
strange-tiger <jinolee9525@gmail.com>
2026-09-08 14:24:24 +0900

    Feat: #4.7 4장 복습 super recap
    localStorage는 정보를 저장하고 불러오고 삭제하는 브라우저가 갖는 작은 DB같은 API
    key랑 value를 함께 검색하고, 삭제하고 더한다. key로 검색하면 value를 string 형태로 받는다.
    preventDefault로 event의 원래 행동을 멈춘다.
    이벤트로 함수를 자바스크립트가 호출하면서 첫 인수로 event에 관한 정보를 준다.

--- momentum/app.js
+++ momentum/app.js
@@ -9,13 +9,13 @@ function onLoginSubmit(event)
 {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
-function paintGreetings(username)
+function paintGreetings()
 {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -31,5 +31,5 @@ if (savedUsername === null)
 else
 {
     // show the greetings
-    paintGreetings(savedUsername);
+    paintGreetings();
 }
