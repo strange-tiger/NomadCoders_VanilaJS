f6631a9b4df77b4c19a36fa139c905aa5c5a191d
strange-tiger <jinolee9525@gmail.com>
2026-09-12 17:31:48 +0900

    Feat: #7.6 Deleting To Dos
    앞선 코드에서 오류를 수정
    ToDos의 요소를 아이디와 내용으로 이루어진 오브젝트로 만든다.

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -8,7 +8,7 @@ let toDos = [];
 
 function saveToDos()
 {
-    localStorage.setItem(TODOS_KEY, JSON.stringify(tDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDO(event)
@@ -20,13 +20,14 @@ function deleteToDO(event)
 function paintToDo(newTodo)
 {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    li.appendChild(span);
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDO)
+    li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
     toDoList.appendChild(li);
 }
 
@@ -35,8 +36,13 @@ function handleToDoSubmit(event)
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj =
+    {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -48,5 +54,5 @@ if (savedToDos != null)
 {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
-    parsedToDos.array.forEach(paintToDo);
+    parsedToDos.forEach(paintToDo);
 }
